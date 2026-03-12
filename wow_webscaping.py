9196b238efb99992723625b1ab9777c4ad637578
Le Luu <le.luu.dev@gmail.com>
2026-03-12 13:54:16 -0400

    Add transform_webscrape_data function for webscraping data

--- wow_webscaping.py
+++ wow_webscaping.py
@@ -65,13 +65,40 @@ def historic_data(MD_TOKEN):
     con = duckdb.connect('md:?motherduck_token=' + MD_TOKEN)
     his_df = con.sql("SELECT * FROM wow_data.wow_historic_data").df()
     con.close()
+
+    his_df['Year_Week'] = his_df['Year'].astype(str) +  his_df['Week'].astype(str).str.zfill(2)
     return his_df
+
+def transform_webscrape_data(webscraped_data_df):
+    #remove the brackets and quotes in the categories and strip whitespace. If Category is empty, replace with "Other"
+    webscraped_data_df['Category'] = webscraped_data_df['categories'].str.replace(r"[\[\]']", "",regex=True).str.strip()
+    webscraped_data_df['Category'] = webscraped_data_df['Category'].replace('', "Other").str.title()
+
+    #Extract the year and week number from the url column and convert it to int
+    webscraped_data_df['Year'] = webscraped_data_df['url'].str.extract(r'.*(\d{4}).*').astype(int)
+    webscraped_data_df['Week'] = webscraped_data_df['url'].str.extract(r'.*w(\d+)[tab|\/].*').astype(int)
+
+    #Create a Year_Week column by concatenating the Year and Week columns, with week number zero-padded to 2 digits
+    webscraped_data_df['Year_Week'] = webscraped_data_df['Year'].astype(str) + webscraped_data_df['Week'].astype(str).str.zfill(2)
+
+    column_rename_mapping = {
+    'title': 'Title',
+    'url': 'Link',
+    'author': 'Author'}
+    webscraped_data_df.rename(columns=column_rename_mapping, inplace=True)
+
+    #Drop some columns to match with the historical data
+    webscraped_data_df.drop(columns=['categories','author_url','title_attribute'], inplace=True)
+    return webscraped_data_df
+
+
 
 def main():
     load_dotenv()
     MD_TOKEN = os.getenv("MD_TOKEN")
     url = "https://www.workout-wednesday.com/latest/"
 
+    #================================KEEP========================================
     # print("===================================================")
     # historic_data_df = historic_data(MD_TOKEN)
     # print("Historic Data:")
@@ -80,6 +107,8 @@ def main():
     # print("===================================================")
     # print("Webscraped Data:")
     # webscraped_df = webscrape_data(url)
+    #========================================================================
+
 
     #========================================================================
     # Transforming data by using the local files
@@ -87,12 +116,16 @@ def main():
     historical_data_df = pd.read_csv("wow_historic_data.csv")
     webscraped_data_df = pd.read_csv("webscraped_data_local.csv")
 
+    historical_data_df['Year_Week'] = historical_data_df['Year'].astype(str) +  historical_data_df['Week'].astype(str).str.zfill(2)
+    webscraped_data_df = transform_webscrape_data(webscraped_data_df)
+
     print("historical data here: ")
     print(historical_data_df.head())
 
     print("===================================================")
     print("webscraped data here: ")
     print(webscraped_data_df.head())
+
 
 if __name__ == "__main__":
     main()
